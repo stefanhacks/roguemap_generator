d3e8a75dc4586c3785be6986129cc2baf718a1b4
Filipe Stefano <filipe.ghisi@gmail.com>
2025-01-23 23:03:02 -0300

    Created simple UI, set main scene.

---
+++ scenes/components/ui.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+@onready var margin_container: MarginContainer = $MarginContainer
+
+var fading = false
+
+func fade_ui() -> void:
+	if fading: return
+	fading = true
+	
+	await get_tree().create_timer(3).timeout
+	var tween = get_tree().create_tween()
+	tween.tween_property(margin_container, "modulate:a", 0, 1)
+	print("hiding")
