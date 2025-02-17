e562efdf9093bb912bb705e919e7833384df4a29
Filipe Stefano <filipe.ghisi@gmail.com>
2025-02-17 20:44:05 -0300

    Minor alterations to better use on mobile.

--- scenes/tests/test_map_generator.gd
+++ scenes/tests/test_map_generator.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const DOUBLE_TAP_DELAY = 0.25
+enum TapStage { GENERATE, ADD, MAKE }
 
 @onready var map_generator: MapGenerator = $MapGenerator
 @onready var screen_size: Vector2i = get_viewport().get_visible_rect().size
@@ -9,6 +10,7 @@ const DOUBLE_TAP_DELAY = 0.25
 
 var tile_map_layer: TileMapLayer
 var double_tap_span = DOUBLE_TAP_DELAY
+var current_stage = TapStage.GENERATE
 
 
 func _ready() -> void:
@@ -20,15 +22,12 @@ func _physics_process(delta: float) -> void:
 	
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("generate") or _is_double_tap(event):
-		ui.fade_ui()
+	if event.is_action_pressed("generate") or _is_double_tap_stage(event, TapStage.GENERATE):
 		_generate_room()
-	elif event.is_action_pressed("make_road"):
-		if tile_map_layer != null:
-			_make_roads()
-	elif event.is_action_pressed("add"):
-		if tile_map_layer != null:
-			_add_random_road()
+	elif event.is_action_pressed("add") or _is_double_tap_stage(event, TapStage.ADD):
+		_add_random_road()
+	elif event.is_action_pressed("make_road") or _is_double_tap_stage(event, TapStage.MAKE):
+		_make_roads()
 
 
 func _is_double_tap(event: InputEvent) -> bool:
@@ -42,14 +41,24 @@ func _is_double_tap(event: InputEvent) -> bool:
 	return false
 
 
+func _is_double_tap_stage(event: InputEvent, stage: TapStage) -> bool:
+	return current_stage == stage and _is_double_tap(event)
+	
+
 func _generate_room() -> void:
+	ui.fade_ui()
 	tile_map_layer.clear()
 	map_generator.make_map(Vector2i(-1, -1), screen_dimensions + Vector2i(1, 2))
-
-
-func _make_roads() -> void:
-	map_generator.make_roads()
+	current_stage = TapStage.ADD
 
 
 func _add_random_road() -> void:
-	map_generator.add_random_road()
+	if tile_map_layer != null:
+		map_generator.add_random_road()
+		current_stage = TapStage.MAKE
+
+
+func _make_roads() -> void:
+	if tile_map_layer != null:
+		map_generator.make_roads()
+		current_stage = TapStage.GENERATE
